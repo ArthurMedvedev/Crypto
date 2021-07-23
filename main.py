be6ee7790556f0d2416d0e66f2ca68a809a48f33
Your Name <you@example.com>
2021-07-23 18:36:04 +0300

    '*'

--- main.py
+++ main.py
@@ -10,29 +10,24 @@ def main():
         SYMBOL = 'ABCDEFGHLIKLMNOPQRSTUVWXYZabcdefghliklmnopqrstuvwxyz1234567890 '
         key = 13
         Cryptotext = ''
-        for symbol in SYMBOL:
+        for symbol in char:
             if symbol in SYMBOL:
                 currentindex = SYMBOL.find(symbol)
                 cryptoindex = currentindex + key
-<<<<<<< HEAD
             if cryptoindex >= len(SYMBOL) - 1:
                 cryptoindex -= len(SYMBOL)
             Cryptotext += SYMBOL[cryptoindex]
-            print(cryptoindex)
-=======
-                if cryptoindex >= len(SYMBOL) - 1:
-                    cryptoindex -= len(SYMBOL)
-
-                    Cryptotext += SYMBOL[cryptoindex]
-        print(Cryptotext)
->>>>>>> 26c28b5cce822b619b69946e3cb5281e196b95c3
-        #return Cryptotext
+        return Cryptotext
 
                 
     if request.method == 'POST':
         char = request.form['find']
-        print(Caesar(char))
-    return render_template(template_name_or_list="index.html")
+        C = Caesar(char)
+    else:
+        C = None
+        
+        
+    return render_template(template_name_or_list="index.html", code=C)
 
 if __name__ == '__main__':
     App.run(host='localhost', port=8000)
