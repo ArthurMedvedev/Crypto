37d973a9802111b7848981e06984466f61bf0e63
Your Name <you@example.com>
2021-08-06 14:11:35 +0300

    '$'

--- лабы 7 семестр/CaesarCipher.py
+++ лабы 7 семестр/CaesarCipher.py
@@ -28,13 +28,13 @@ def text_caesar(plain_text: str, shift: int) -> str:
 def decrypt_text_caesar(plain_text: str, shift: int) ->str:
     return text_caesar(plain_text, -shift)
 
-'''t = int(input('key: '))
+t = int(input('key: '))
 
 if t in range(61, 70):
-     print(text_caesar('A', t))
-     print(decrypt_text_caesar('N', t))
+     print(text_caesar(" Mind you, I don't believe these rumours at all. At least, I can't believe them when I see you. Sin is a thing that writes itself across a man's face. It cannot be concealed. People talk sometimes of secret vices. There are no such things.", t))
+     print(decrypt_text_caesar("Xtyo jzf, T ozy'e mpwtpgp espdp cfxzfcd le lww. Le wplde, T nly'e mpwtpgp espx hspy T dpp jzf. Dty td l estyr esle hctepd tedpwq lnczdd l xly'd qlnp. Te nlyyze mp nzynplwpo. Apzawp elwv dzxpetxpd zq dpncpe gtnpd. Espcp lcp yz dfns estyrd", t))
 else:
-    print(False)'''
+    print(False)
 
     
 
--- лабы 7 семестр/decryptcesar.py
+++ лабы 7 семестр/decryptcesar.py
@@ -7,23 +7,15 @@ def generate_notepad_from_two_lists(lst1: list, lst2: list) -> dict:
     return {k: v for v, k in zip(lst1, lst2)}
 
 
-T = get('https://www.weblitera.com/book/?id=19&lng=1&ch=7&l=ru').text
-E = text_caesar(T, 1)
+T = get('https://www.weblitera.com/book/?id=19&lng=1&ch=12&l=ru').text
+E = text_caesar(T, 45)
 lst = generate_list_for_frequency_analyse()[0]
 #print(lst)
-lst_encrypt = [x for x in text_caesar(lst, shift=1)]
-#print(lst_encrypt)
+lst_encrypt = [x for x in text_caesar(lst, shift=45)]
+print(lst_encrypt)
 
-'''shablon = []
 
-for i, v in enumerate(lst_encrypt):
-    
-    #lst_encrypt[i] = lst[i]
-    shablon.append(lst[i])
-
-#print(shablon)
-#print(lst_encrypt)'''
-D = generate_notepad_from_two_lists(lst_encrypt, shablon)
+D = generate_notepad_from_two_lists(lst_encrypt, lst)
 
 
 for key, value in D.items():
@@ -38,22 +30,11 @@ for key, value in D.items():
 print(KEY, VALUE)
 
 shift = VALUE - KEY
+
 print(shift)
-
-
-
-
-
-'''N = ''
-
-for i in T:
-    
-    try:
-        N += generate_notepad_from_two_lists(shablon, lst_encrypt)[i]
-    except:
-        pass
-'''
 print(decrypt_text_caesar(E, shift))
-#print(N)
+
+#print(Counter(E))
+
 
 
